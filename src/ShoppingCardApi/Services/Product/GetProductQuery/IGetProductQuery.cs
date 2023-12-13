89773b3b7c539fa896330702638086d0da3baf55
mr.ahmadi <Manv.ma123@gmail.com>
2023-12-13 10:15:06 +0330

    ref: remove services

--- src/ShoppingCardApi/Services/Product/GetProductQuery/IGetProductQuery.cs
+++
@@ -1,6 +0,0 @@
-namespace ShoppingCartApi.UseCases.Product.GetProductQuery;
-
-public interface IGetProductQuery
-{
-    Task<Domain.Product> GetProductService(long id);
-}
--- src/ShoppingCardApi/Services/ShoppingCart/IGetProductFromCartQuery.cs
+++
@@ -1,6 +0,0 @@
-namespace ShoppingCartApi.UseCases.ShoppingCart.AddProductToCart.Queries;
-
-public interface IGetProductFromCartQuery
-{
-    Task<Domain.ShoppingCart> GetShoppingCartService(long id);
-}
