1cc220bcaa86be7b9915de8480d1fdff64adec8b
mrAhmadi <mrAhmadi@DESKTOP-N355MJ7>
2023-12-20 00:36:16 +0330

    chore: base class test

---
+++ src/Tests/ShoppingCart.Tests.Base/ShoppingCartBuilder.cs
@@ -0,0 +1,30 @@
+﻿namespace ShoppingCart.Write.Tests.Base;
+public class ShoppingCartBuilder
+{
+
+
+    public int Quantity { get; private set; }
+
+    public int TotalPrice { get; private set; }
+
+    private ShoppingCartBuilder(){}
+
+
+    public ShoppingCartBuilder init()
+    {
+        return new ShoppingCartBuilder();
+    }
+
+    public ShoppingCartBuilder WithQuantity(int quantity)
+    {
+        Quantity = quantity;
+        return this;
+    }
+
+    public ShoppingCartBuilder WhithTotalPrice(int totalPrice)
+    {
+        TotalPrice = totalPrice;
+        return this;
+    }
+}
+
